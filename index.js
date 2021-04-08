ad37b46fc646f5b448b312b2c38b47a5e69db92b
M aldi Prasetyo <prasetyoaldi874@gmail.com>
2021-04-08 20:00:44 +0700

    Fouuth Project

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
-const express = require ('express')
+const { Console } = require('console')
+const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const bodyParser= require('body-parser')
+const mongoose= require ('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/latihan', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then (() => {
+    console.log('connected')
+}).catch((e)=>{
+    console.log(e)
+    console.log('unconnected')
+})
 
 app.use(bodyParser.json({
-    extends: true,
+    extend: true,
     limit: '20mb'
 }))
 
 app.use(bodyParser.urlencoded({
-    extended: true,
+    extend: true,
     limit: '20mb'
 }))
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+app.get('/', (req, res)=>{
+    res.send('<h1>hello world 1</h1>')
+
 })
 
-app.get('/profile/:username/:id', (req, res) =>{
+app.get('/profile/:username/:id', (req, res)=>{
     console.log(req.params)
-    res.send('Username Anda ' + req.params.username)
-
+    res.send('username = '+ req.params.username)
+})
+//req params
+app.get('/daerah/:daerah', (req, res)=>{
+    console.log(req.params)
+    res.send('daerah anda = '+ req.params.daerah)
 })
 
-app.get('/daerah/:namaDaerah/:id', (req, res) =>{
-    const namaDaerah = req.params.namaDaerah
-    const idDaerah = req.params.id
-    res.send('Daerah Anda ' + namaDaerah + ' Id daerah = ' + idDaerah)
+//req body
+//app.post('/register', (req, res)=> {
+//    console.log(req.body)
+    //res.json(req.body)
+//})
+app.use('/user/', require('./routes/User'))
 
-})
-
-app.post('/register',(req, res) =>{
-    console.log(req.body)
-    res.json(req.body)
-})
-
-app.listen(3000, () =>{
-    console.log('Server Mulai')
+app.listen(3000, () => {
+    console.log('server started')
 })
